c6e998e49df7a604ef86f6330da3c3ac13d5f1a2
Zain <zainnhlawy@gmail.com>
2025-11-03 15:17:41 +0200

    the meal project

---
+++ lib/screen/categories.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:meals_managment/data/category_dummy_data.dart';
+import 'package:meals_managment/widgets/category_grid_item.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('choose your categories',)),
+      body: GridView(
+        padding: EdgeInsets.all(20),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
